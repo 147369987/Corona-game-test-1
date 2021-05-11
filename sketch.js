f6c9ea634a60028e15ce3683d94791b49d84082d
Niraaghan S <78425250+147369987@users.noreply.github.com>
2021-05-11 18:15:40 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -336,4 +336,10 @@ function spawnSan(){
   }
  }
 
+function isTouchDevice() {
+        return (('ontouchstart' in window) ||
+           (navigator.maxTouchPoints > 0) ||
+           (navigator.msMaxTouchPoints > 0));
+      }
+
  
